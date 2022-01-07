ceffadd2bf22773beb7977280ab9fd8d4f914982
Damian Sire <damiansire@hotmail.com>
2022-01-06 23:03:11 -0300

    Add bot

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-require("dotenv").config();
-const { DISCORD_TOKEN } = process.env;
+const { Client } = require("discord.js");
+const { clientId, guildId, token } = require("./config.json");
+const commands = require("./commands.json");
+
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+client.on("ready", () => {
+  console.log("Bot Now connected!");
+  console.log("Logged In as", client.user.tag);
+  client.user.setStatus("dnd"); // online, idle, invisible, dnd
+  console.log("Bot status: ", client.user.presence.status);
+});
+
+client.on("messageCreate", (msg) => {
+  if (!msg.author.bot) {
+    const command = commands.find((command) =>
+      msg.content.includes(command.prefix)
+    );
+
+    if (command) {
+      msg.channel.send(command.msg);
+    }
+  }
+});
+
+client.login(token);
+
+//msg.content es el contenido del mensaje
